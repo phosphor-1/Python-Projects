7d4b1ddf4ceee8f86cb91e1d4c28f9a2aa394ee1
phosphor-1 <isaacpaintball@gmail.com>
2022-06-24 12:51:47 -0700

    .

---
+++ python-practice/ex2.py
@@ -0,0 +1,4 @@
+#The exercise comes first (with a few extras if you want the extra challenge or want to spend more time), followed by a discussion. Enjoy!
+#Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. 
+#Hint: how does an even / odd number react differently when divided by 2?
+
